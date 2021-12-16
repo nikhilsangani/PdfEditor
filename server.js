0c176f67f3d897fe1e0923a84c33423945d8f2fa
Nikhil Sangani <nikhil2545sangani@gmail.com>
2021-12-16 20:56:19 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-//Install express server
 const express = require('express');
+const path = require('path');
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default Heroku port
+app.use(express.static(__dirname + '/dist/PdfEditor'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/PdfEditor/index.html'));});
 app.listen(process.env.PORT || 8080);
